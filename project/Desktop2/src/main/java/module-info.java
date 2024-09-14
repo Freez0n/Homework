b77b435710bbab896dadee953bc48b3529a900ad
Freez0n <andrei3829812005@gmail.com>
2024-09-14 12:13:47 +0300

    feat: удаление лишнего проекта

--- project/Desktop2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.desktop2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.desktop2 to javafx.fxml;
-    exports com.example.desktop2;
-}
